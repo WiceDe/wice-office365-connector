9f5fd5ab2b2cd2623504a5bc9bd4e7f03a86513a
Shterion <shterion.yanev@yahoo.com>
2019-05-13 11:26:50 +0200

    Getting headers in xml

--- function-file/function-file.js
+++ function-file/function-file.js
@@ -14,8 +14,147 @@ function showError(error) {
 
 var settingsDialog;
 
+function getHeadersRequest(id) {
+    // Return a GetItem EWS operation request for the headers of the specified item.
+    return "<GetItem xmlns='http://schemas.microsoft.com/exchange/services/2006/messages'>" +
+        "  <ItemShape>" +
+        "    <t:BaseShape>IdOnly</t:BaseShape>" +
+        "    <t:BodyType>Text</t:BodyType>" +
+        "    <t:AdditionalProperties>" +
+        // PR_TRANSPORT_MESSAGE_HEADERS
+        "      <t:ExtendedFieldURI PropertyTag='0x007D' PropertyType='String' />" +
+        "    </t:AdditionalProperties>" +
+        "  </ItemShape>" +
+        "  <ItemIds><t:ItemId Id='" + id + "'/></ItemIds>" +
+        "</GetItem>";
+}
+
+function getSoapEnvelope(request) {
+    // Wrap an Exchange Web Services request in a SOAP envelope.
+    return "<?xml version='1.0' encoding='utf-8'?>" +
+        "<soap:Envelope xmlns:soap='http://schemas.xmlsoap.org/soap/envelope/'" +
+        "               xmlns:t='http://schemas.microsoft.com/exchange/services/2006/types'>" +
+        "  <soap:Header>" +
+        "     <t:RequestServerVersion Version='Exchange2013'/>" +
+        "  </soap:Header>" +
+        "  <soap:Body>" +
+        request +
+        "  </soap:Body>" +
+        "</soap:Envelope>";
+}
+
+function callbackEws(asyncResult, headersLoadedCallback) {
+    // try {
+        // Process the returned response here.
+        var header = null;
+        if (asyncResult.value) {
+            header = extractHeadersFromXml(asyncResult.value);
+            console.log('HEADER: ', header);
+
+            // We might not have a prop and also no error. This is OK if the prop is just missing.
+            if (header && !header.prop) {
+                if (header.responseCode && header.responseCode.length > 0 && header.responseCode[0].firstChild && header.responseCode[0].firstChild.data === "NoError") {
+                    // headersLoadedCallback(null, "EWS");
+                    // ShowError(null, ImportedStrings.mha_headersMissing, true);
+                    return;
+                }
+            }
+        }
+
+        if (header && header.prop) {
+            headersLoadedCallback(header.prop, "EWS");
+        }
+        else {
+            throw new Error(ImportedStrings.mha_requestFailed);
+        }
+    // }
+    // catch (e) {
+    //     if (asyncResult) {
+    //         LogError(null, "Async Response\n" + stripHeaderFromXml(JSON.stringify(asyncResult, null, 2)));
+    //     }
+    //
+    //     if (logResponse) {
+    //         LogError(null, "Original Response\n" + stripHeaderFromXml(JSON.stringify(logResponse, null, 2)));
+    //     }
+    //
+    //     headersLoadedCallback(null, "EWS");
+    //     ShowError(e, "EWS callback failed");
+    // }
+}
+
+function extractHeadersFromXml(xml) {
+    // This function plug in filters nodes for the one that matches the given name.
+    // This sidesteps the issues in jquery's selector logic.
+    (function ($) {
+        $.fn.filterNode = function (node) {
+            return this.find("*").filter(function () {
+                return this.nodeName === node;
+            });
+        };
+    })(jQuery);
+
+    var ret = {};
+    try {
+        // Strip encoded embedded null characters from our XML. parseXML doesn't like them.
+        xml = xml.replace(/&#x0;/g, "");
+        var response = $.parseXML(xml);
+        var responseDom = $(response);
+
+        if (responseDom) {
+            // See http://stackoverflow.com/questions/853740/jquery-xml-parsing-with-namespaces
+            // See also http://www.steveworkman.com/html5-2/javascript/2011/improving-javascript-xml-node-finding-performance-by-2000
+            // We can do this because we know there's only the one property.
+            var extendedProperty = responseDom.filterNode("t:ExtendedProperty");
+            if (extendedProperty.length > 0) {
+                ret.prop = extendedProperty[0].textContent;
+            }
+        }
+
+        if (!ret.prop) {
+            ret.responseCode = responseDom.filterNode("m:ResponseCode");
+        }
+    } catch (e) {
+        // Exceptions thrown from parseXML are super chatty and we do not want to log them.
+        // We throw this exception away and just return nothing.
+    }
+
+    return ret;
+}
+
 function saveMailInWice(event) {
+  // console.log('MAIL ITEM: ', Office.context.mailbox.item);
+
+  var itemHeaderRequest = getHeadersRequest(Office.context.mailbox.item.itemId);
+  var envelope = getSoapEnvelope(itemHeaderRequest);
+  Office.context.mailbox.makeEwsRequestAsync(envelope, function (asyncResult) {
+      // console.log('HERE');
+      // console.log(asyncResult);
+      callbackEws(asyncResult, null);
+  });
+
+  Office.context.mailbox.getCallbackTokenAsync({ isRest: true }, function (result) {
+    // console.log(result);
+    // getHeaders(result.value, null);
+    // try {
+    //     if (result.status === "succeeded") {
+    //         var accessToken = result.value;
+    //         console.log('FLAG 1');
+    //         getHeaders(accessToken, headersLoadedCallback);
+    //     } else {
+    //         LogError(null, 'Unable to obtain callback token.\nFallback to EWS.\n' + JSON.stringify(result, null, 2), true);
+    //         sendHeadersRequestEWS(headersLoadedCallback);
+    //     }
+    // }
+    // catch (e) {
+    //     ShowError(e, "Failed in getCallbackTokenAsync");
+    // }
+});
+
+  // var schema = "http://schemas.microsoft.com/mapi/proptag/0x007D001E";
+  // var headers = Office.context.mailbox.item.PropertyAccessor.GetProperty(schema);
+  // console.log(headers);
   config = getConfig();
+  // console.log(config);
 
   var emptyWiceServer = $.isEmptyObject(config.wiceServer);
   var emptyMandant = $.isEmptyObject(config.mandant);
@@ -30,11 +169,9 @@ function saveMailInWice(event) {
         asyncContext: "This is passed to the callback"
       },
       function callback(result) {
+        // console.log('HTML FORMAT: ', result);
         var url = config.wiceServer + "/pserv/base/thunderbird";
         // TODO: Save email in wice
-        console.log(result.value);
-        console.log(config);
-        // console.log('URL: ', url);
         // Save mail functions is in helpers
         saveMail();
       });
