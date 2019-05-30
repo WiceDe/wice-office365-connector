139ee351c2d77eee35f12dd299f17007826c50f8
Shterion <shterion.yanev@yahoo.com>
2019-05-30 10:50:16 +0200

    Clean up

--- function-file/function-file.js
+++ function-file/function-file.js
@@ -14,112 +14,6 @@ function showError(error) {
 
 var settingsDialog;
 
-// function getHeadersRequest(id) {
-//     // Return a GetItem EWS operation request for the headers of the specified item.
-//     return "<GetItem xmlns='http://schemas.microsoft.com/exchange/services/2006/messages'>" +
-//         "  <ItemShape>" +
-//         "    <t:BaseShape>IdOnly</t:BaseShape>" +
-//         "    <t:BodyType>Text</t:BodyType>" +
-//         "    <t:AdditionalProperties>" +
-//         // PR_TRANSPORT_MESSAGE_HEADERS
-//         "      <t:ExtendedFieldURI PropertyTag='0x007D' PropertyType='String' />" +
-//         "    </t:AdditionalProperties>" +
-//         "  </ItemShape>" +
-//         "  <ItemIds><t:ItemId Id='" + id + "'/></ItemIds>" +
-//         "</GetItem>";
-// }
-
-// function getSoapEnvelope(request) {
-//     // Wrap an Exchange Web Services request in a SOAP envelope.
-//     return "<?xml version='1.0' encoding='utf-8'?>" +
-//         "<soap:Envelope xmlns:soap='http://schemas.xmlsoap.org/soap/envelope/'" +
-//         "               xmlns:t='http://schemas.microsoft.com/exchange/services/2006/types'>" +
-//         "  <soap:Header>" +
-//         "     <t:RequestServerVersion Version='Exchange2013'/>" +
-//         "  </soap:Header>" +
-//         "  <soap:Body>" +
-//         request +
-//         "  </soap:Body>" +
-//         "</soap:Envelope>";
-// }
-
-// function callbackEws(asyncResult, headersLoadedCallback) {
-//     // try {
-//     // Process the returned response here.
-//     var header = null;
-//     if (asyncResult.value) {
-//         header = extractHeadersFromXml(asyncResult.value);
-//         // console.log('HEADER: ', header);
-//
-//         // We might not have a prop and also no error. This is OK if the prop is just missing.
-//         if (header && !header.prop) {
-//             if (header.responseCode && header.responseCode.length > 0 && header.responseCode[0].firstChild && header.responseCode[0].firstChild.data === "NoError") {
-//                 // headersLoadedCallback(null, "EWS");
-//                 // ShowError(null, ImportedStrings.mha_headersMissing, true);
-//                 return;
-//             }
-//         }
-//     }
-//
-//     if (header && header.prop) {
-//         headersLoadedCallback(header.prop, "EWS");
-//     } else {
-//         throw new Error(ImportedStrings.mha_requestFailed);
-//     }
-// }
-// catch (e) {
-//     if (asyncResult) {
-//         LogError(null, "Async Response\n" + stripHeaderFromXml(JSON.stringify(asyncResult, null, 2)));
-//     }
-//
-//     if (logResponse) {
-//         LogError(null, "Original Response\n" + stripHeaderFromXml(JSON.stringify(logResponse, null, 2)));
-//     }
-//
-//     headersLoadedCallback(null, "EWS");
-//     ShowError(e, "EWS callback failed");
-// }
-// }
-
-// function extractHeadersFromXml(xml) {
-//     // This function plug in filters nodes for the one that matches the given name.
-//     // This sidesteps the issues in jquery's selector logic.
-//     (function($) {
-//         $.fn.filterNode = function(node) {
-//             return this.find("*").filter(function() {
-//                 return this.nodeName === node;
-//             });
-//         };
-//     })(jQuery);
-//
-//     var ret = {};
-//     try {
-//         // Strip encoded embedded null characters from our XML. parseXML doesn't like them.
-//         xml = xml.replace(/&#x0;/g, "");
-//         var response = $.parseXML(xml);
-//         var responseDom = $(response);
-//
-//         if (responseDom) {
-//             // See http://stackoverflow.com/questions/853740/jquery-xml-parsing-with-namespaces
-//             // See also http://www.steveworkman.com/html5-2/javascript/2011/improving-javascript-xml-node-finding-performance-by-2000
-//             // We can do this because we know there's only the one property.
-//             var extendedProperty = responseDom.filterNode("t:ExtendedProperty");
-//             if (extendedProperty.length > 0) {
-//                 ret.prop = extendedProperty[0].textContent;
-//             }
-//         }
-//
-//         if (!ret.prop) {
-//             ret.responseCode = responseDom.filterNode("m:ResponseCode");
-//         }
-//     } catch (e) {
-//         // Exceptions thrown from parseXML are super chatty and we do not want to log them.
-//         // We throw this exception away and just return nothing.
-//     }
-//
-//     return ret;
-// }
-
 function saveMailInWice() {
     // var itemHeaderRequest = getHeadersRequest(Office.context.mailbox.item.itemId);
     // var envelope = getSoapEnvelope(itemHeaderRequest);
@@ -232,9 +126,6 @@ function saveMailInWice() {
             saveMail(result, url, cookie);
             // Process the returned response here.
         }
-
-        // sendRequest();
-
     } else {
         // Save the event object so we can finish up later
         btnEvent = event;
